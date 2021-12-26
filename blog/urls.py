3514d2e2ff3cd82d7e47d23b5c765d76f03ec43a
habtamu <habtetsegaye19@gmail..com>
2021-12-26 05:19:50 +0300

    form is addd

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
-from .views import BlogHomeView,BlogDetailView
-
+from .views import (
+BlogListView,
+BlogDetailView,
+BlogCreateView,
+BlogUpdateView,
+BlogDeleteView# new
+)
 urlpatterns = [
-    path ('', BlogHomeView.as_view(), name= "home"),
-    path('post/<int:pk>/', BlogDetailView.as_view(),
-name='post_detail'),
-
+    path('post/<int:pk>/delete/', # new
+BlogDeleteView.as_view(), name='post_delete'),
+path('post/<int:pk>/edit/',
+BlogUpdateView.as_view(), name='post_edit'), # new
+path('post/new/', BlogCreateView.as_view(), name='post_new'),
+path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
+path('', BlogListView.as_view(), name='home'),
 ]
-
